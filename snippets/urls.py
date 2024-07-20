ef835b9cda137286375b5446d8d6665fa910353d
tmalik258 <talhamalik25.tm@gmail.com>
2024-07-20 16:27:56 +0500

    Part 2 of tutorial completed, added api_view decorator for func based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets.views import snippet_detail, snippet_list
 
 
@@ -7,3 +9,5 @@ urlpatterns = [
 	path('', snippet_list, name='snippet_list'),
 	path('<int:pk>/', snippet_detail, name='snippet_detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
